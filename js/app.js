cb065f1364eb5ef6451a53bbdf42b301c2e8bcde
Andrew A Lee <andrew@andrewleedev.com>
2026-05-22 16:48:44 -0700

    Implements base solution

--- js/app.js
+++ js/app.js
@@ -1,97 +1,88 @@
-import Dialer from "./dialer.js";
+import Dialer from './dialer.js';
 
+document.addEventListener('DOMContentLoaded', function ready() {
+  const enterHopsEl = document.getElementById('enter-hop-count');
+  const dialpadEl = document.getElementById('dialpad');
+  const dialpadKeyEls = dialpadEl.querySelectorAll('button');
+  const resultsEl = document.getElementById('results');
+  const startingKey1El = document.getElementById('starting-key-1');
+  const startingKey2El = document.getElementById('starting-key-2');
+  const hopCountEl = document.getElementById('hop-count');
+  const pathCountEl = document.getElementById('path-count');
+  const countTimingEl = document.getElementById('count-timing');
+  const acyclicPathsEl = document.getElementById('acyclic-paths');
+  const pathsTimingEl = document.getElementById('paths-timing');
 
-document.addEventListener("DOMContentLoaded",function ready(){
-	var enterHopsEl = document.getElementById("enter-hop-count");
-	var dialpadEl = document.getElementById("dialpad");
-	var dialpadKeyEls = dialpadEl.querySelectorAll("button");
-	var resultsEl = document.getElementById("results");
-	var startingKey1El = document.getElementById("starting-key-1");
-	var startingKey2El = document.getElementById("starting-key-2");
-	var hopCountEl = document.getElementById("hop-count");
-	var pathCountEl = document.getElementById("path-count");
-	var countTimingEl = document.getElementById("count-timing");
-	var acyclicPathsEl = document.getElementById("acyclic-paths");
-	var pathsTimingEl = document.getElementById("paths-timing");
+  dialpadEl.addEventListener('mouseover', onHoverKey, false);
+  dialpadEl.addEventListener('mouseout', onHoverKey, false);
+  dialpadEl.addEventListener('click', onClickKey, false);
 
-	dialpadEl.addEventListener("mouseover",onHoverKey,false);
-	dialpadEl.addEventListener("mouseout",onHoverKey,false);
-	dialpadEl.addEventListener("click",onClickKey,false);
+  // ********************************
 
+  function onHoverKey(evt) {
+    const el = evt.target;
 
-	// ********************************
+    // discover which keys (if any) can be reached from
+    // a hovered key
+    const reachable = el.matches('button:hover')
+      ? Dialer.reachableKeys(Number(el.value))
+      : [];
 
-	function onHoverKey(evt) {
-		var el = evt.target;
+    for (let keyEl of dialpadKeyEls) {
+      if (reachable.includes(Number(keyEl.value))) {
+        keyEl.classList.add('highlighted');
+      } else {
+        keyEl.classList.remove('highlighted');
+      }
+    }
+  }
 
-		// discover which keys (if any) can be reached from
-		// a hovered key
-		var reachable = (
-			el.matches("button:hover") ?
-				Dialer.reachableKeys(Number(el.value)) :
-				[]
-		);
+  function onClickKey(evt) {
+    const clickedEl = evt.target;
 
-		for (let keyEl of dialpadKeyEls) {
-			if (reachable.includes(Number(keyEl.value))) {
-				keyEl.classList.add("highlighted");
-			}
-			else {
-				keyEl.classList.remove("highlighted");
-			}
-		}
-	}
+    // clicked on a dialpad key?
+    if (clickedEl.matches('#dialpad > button')) {
+      let startingKey = Number(clickedEl.value);
+      startingKey1El.innerText = startingKey;
+      startingKey2El.innerText = startingKey;
 
-	function onClickKey(evt) {
-		var clickedEl = evt.target;
+      let hopCount = Number(enterHopsEl.value) || 1;
+      enterHopsEl.value = hopCount;
+      hopCountEl.innerText = hopCount;
 
-		// clicked on a dialpad key?
-		if (clickedEl.matches("#dialpad > button")) {
-			let startingKey = Number(clickedEl.value);
-			startingKey1El.innerText = startingKey;
-			startingKey2El.innerText = startingKey;
+      let startTiming = performance.now();
+      // count the distinct paths (including cyclic paths)
+      let pathCount = Dialer.countPaths(startingKey, hopCount);
+      let endTiming = performance.now();
+      printTiming(countTimingEl, Number(endTiming - startTiming) || 0);
+      pathCountEl.innerText = pathCount;
 
-			let hopCount = Number(enterHopsEl.value) || 1;
-			enterHopsEl.value = hopCount;
-			hopCountEl.innerText = hopCount;
+      startTiming = performance.now();
+      let acyclicPaths = Dialer.listAcyclicPaths(startingKey);
+      endTiming = performance.now();
+      printTiming(pathsTimingEl, Number(endTiming - startTiming) || 0);
+      if (acyclicPaths.length > 0) {
+        acyclicPathsEl.innerHTML = '';
+        for (const path of acyclicPaths) {
+          let pathEl = document.createElement('div');
+          pathEl.innerHTML = path.join(' &#x27A1; ');
+          acyclicPathsEl.appendChild(pathEl);
+        }
+      } else {
+        acyclicPathsEl.innerHTML = '-- none --';
+      }
 
+      resultsEl.classList.remove('hidden');
+    }
+  }
 
-			let startTiming = performance.now();
-			// count the distinct paths (including cyclic paths)
-			let pathCount = Dialer.countPaths(startingKey,hopCount);
-			let endTiming = performance.now();
-			printTiming(countTimingEl,Number(endTiming - startTiming) || 0);
-			pathCountEl.innerText = pathCount;
+  function printTiming(timingEl, ms) {
+    ms = Number(ms.toFixed(1));
 
-			startTiming = performance.now();
-			let acyclicPaths = Dialer.listAcyclicPaths(startingKey);
-			endTiming = performance.now();
-			printTiming(pathsTimingEl,Number(endTiming - startTiming) || 0);
-			if (acyclicPaths.length > 0) {
-				acyclicPathsEl.innerHTML = "";
-				for (let path of acyclicPaths) {
-					let pathEl = document.createElement("div");
-					pathEl.innerHTML = path.join(" &#x27A1; ");
-					acyclicPathsEl.appendChild(pathEl);
-				}
-			}
-			else {
-				acyclicPathsEl.innerHTML = "-- none --";
-			}
-
-			resultsEl.classList.remove("hidden");
-		}
-	}
-
-	function printTiming(timingEl,ms) {
-		ms = Number(ms.toFixed(1));
-
-		if (ms >= 50) {
-			timingEl.innerHTML = `(<strong>${(ms / 1000).toFixed(2)}</strong> sec)`;
-		}
-		else {
-			timingEl.innerHTML = `(<strong>${ms.toFixed(1)}</strong> ms)`
-		}
-	}
-
+    if (ms >= 50) {
+      timingEl.innerHTML = `(<strong>${(ms / 1000).toFixed(2)}</strong> sec)`;
+    } else {
+      timingEl.innerHTML = `(<strong>${ms.toFixed(1)}</strong> ms)`;
+    }
+  }
 });
--- js/dialer.js
+++ js/dialer.js
@@ -1,39 +1,91 @@
-export default {
-	reachableKeys,
-	countPaths,
-	listAcyclicPaths
-};
+/*
+1 2 3
+4 5 6
+7 8 9
+  0
+*/
+/*
+3 Hops from 1
+1
+6 8
+0 1 7 1 3
+4 6 6 8 2 6 6 8 4 8
+*/
 
-
-// ****************************
-
+/** @type {(startingDigit: number): number[]} */
 function reachableKeys(startingDigit) {
-	// TODO: return which digits a Knight's move
-	// can hop to from a given starting digit/key
-	//
-	// e.g. 3 -> [ 4, 8 ]
-	//      4 -> [ 3, 9, 0 ]
-	//      5 -> []
-	return [];
+	const keys = [
+    [4, 6],
+    [6, 8],
+    [7, 9],
+    [4, 8],
+    [0, 3, 9],
+    [],
+    [0, 1, 7],
+    [2, 6],
+    [1, 3],
+    [2, 4],
+  ];
+
+  return keys[startingDigit];
 }
 
-function countPaths(startingDigit,hopCount) {
-	// TODO: given the digit/key to start from and
-	// the number of hops to take, return a count
-	// of all the possible paths that could be
-	// traversed
-	return 0;
+/** @type {(startingDigit: number, hopCount: number) => number} */
+function countPaths(startingDigit, hopCount) {
+  if (hopCount === 0) {
+    return 1;
+  }
+
+  const keys = reachableKeys(startingDigit);
+  let count = 0;
+
+  for (const key of keys) {
+    count += countPaths(key, hopCount - 1);
+  }
+
+  return count;
 }
 
+/** @type {(digit: number, seen: boolean[], path: number[], result: number[][])} */
+function traversePath(digit, seen, path, result) {
+  const keys = reachableKeys(digit);
+  let isExhausted = true;
+
+  for (const key of keys) {
+    if (seen[key]) {
+      continue;
+    }
+
+    isExhausted = false;
+    path.push(key);
+    seen[key] = true;
+
+    traversePath(key, seen, path, result);
+
+    path.pop();
+    seen[key] = false;
+  }
+
+  if (isExhausted) {
+    result.push([...path]);
+  }
+}
+
+/** @type {(startingDigit: number) => number[][]} */
 function listAcyclicPaths(startingDigit) {
-	// TODO: given the digit/key to start from,
-	// return a list of the distinct acyclic
-	// paths that are possible to traverse
-	//
-	// e.g. [
-	//   [4, 3, 8, 1, 6, 7, 2, 9],
-	//   [4, 3, 8, 1, 6, 0],
-	//   ...
-	// ]
-	return [];
+  /** @type {boolean[]} */
+  const seen = new Array(10).fill(false);
+  /** @type {number[][]} */
+  const result = [];
+
+  seen[startingDigit] = true;
+  traversePath(startingDigit, seen, [startingDigit], result);
+
+  return result;
 }
+
+export default {
+  reachableKeys,
+  countPaths,
+  listAcyclicPaths,
+};
