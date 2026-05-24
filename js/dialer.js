2756365d861c84371a233ed1442ae3a8db96130c
Andrew A Lee <andrew@andrewleedev.com>
2026-05-23 17:06:51 -0700

    Refactors solution to DP

--- js/dialer.js
+++ js/dialer.js
@@ -1,49 +1,113 @@
-/*
-1 2 3
-4 5 6
-7 8 9
-  0
-*/
-/*
-3 Hops from 1
-1
-6 8
-0 1 7 1 3
-4 6 6 8 2 6 6 8 4 8
-*/
+const dialPad = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [null, 0, null],
+];
 
-/** @type {(startingDigit: number): number[]} */
+const moves = [
+  [-2, -1],
+  [-1, -2],
+  [-2, 1],
+  [-1, 2],
+  [1, 2],
+  [2, 1],
+  [2, -1],
+  [1, -2],
+];
+
+/** @type {Map<number, number[]>} */
+const keysMemo = new Map();
+
+/** @type {(startingDigit: number) => number[]} */
 function reachableKeys(startingDigit) {
-	const keys = [
-    [4, 6],
-    [6, 8],
-    [7, 9],
-    [4, 8],
-    [0, 3, 9],
-    [],
-    [0, 1, 7],
-    [2, 6],
-    [1, 3],
-    [2, 4],
-  ];
+  if (keysMemo.has(startingDigit)) {
+    return keysMemo.get(startingDigit);
+  }
 
-  return keys[startingDigit];
+  const rowLen = dialPad.length;
+  const colLen = dialPad[0].length;
+
+  let row = -1;
+  let col = -1;
+
+  for (let i = 0; i < rowLen; i++) {
+    for (let j = 0; j < colLen; j++) {
+      if (dialPad[i][j] === startingDigit) {
+        row = i;
+        col = j;
+        break;
+      } 
+    }
+  }
+
+  /** @type {number[]} */
+  const moveToKeys = [];
+
+  for (const move of moves) {
+    const nRow = row + move[0];
+    const nCol = col + move[1];
+
+    if (
+      nRow >= 0 && nRow < rowLen
+      && nCol >= 0 && nCol < colLen
+      && dialPad[nRow][nCol] !== null
+    ) {
+      moveToKeys.push(dialPad[nRow][nCol]);
+    }
+  }
+
+  keysMemo.set(startingDigit, moveToKeys);
+  return moveToKeys;
 }
 
-/** @type {(startingDigit: number, hopCount: number) => number} */
-function countPaths(startingDigit, hopCount) {
+/** @type {(startingDigit: number, hopCount: number, memo: Map<string, number>) => number} */
+function countPaths(startingDigit, hopCount, memo = new Map()) {
+  // Recursive/Top-down Solution
+  /*
+  // Base case: exhausted hops
   if (hopCount === 0) {
     return 1;
+  }
+
+  const entry = `${startingDigit},${hopCount}`;
+  if (memo.has(entry)) {
+    return memo.get(entry);
   }
 
   const keys = reachableKeys(startingDigit);
   let count = 0;
 
   for (const key of keys) {
-    count += countPaths(key, hopCount - 1);
+    count += countPaths(key, hopCount - 1, memo);
   }
 
+  memo.set(entry, count);
   return count;
+  */
+
+  // Tabulation/Bottom-up Solution
+  const digitLen = 10;
+  /** @type {number[]} */
+  let prevRow = new Array(digitLen).fill(1);
+
+  // 5 key is always 0 as it's unreachable
+  prevRow[5] = 0;
+
+  // Compute the remaining values
+  for (let i = 0; i < hopCount; i++) {
+    /** @type {number[]} */
+    const currRow = [];
+
+    for (let j = 0; j < digitLen; j++) {
+      const keys = reachableKeys(j);
+      currRow[j] = keys.reduce((sum, key) => sum + prevRow[key], 0);
+    }
+
+    prevRow = currRow;
+  }
+
+  return prevRow[startingDigit];
 }
 
 /** @type {(digit: number, seen: boolean[], path: number[], result: number[][])} */
